d14771a248b321b7f7dc7ac6d736123a4109b955
Spencer Sanchez <spsanchez@wayfair.com>
2018-12-01 20:59:15 -0500

    Add plainText field to graph; update flat data sent to Algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
-require('dotenv').config({
+require(`dotenv`).config({
   path: `.env`
 });
+
+if (!process.env.ALGOLIA_APPID || !process.env.ALGOLIA_APIKEY || !process.env.ALGOLIA_ADMIN_APIKEY) {
+  throw new Error('Missing Algolia config')
+}
 
 // gatsby-config.js
 const query = `{
@@ -10,11 +14,13 @@ const query = `{
         frontmatter {
           title
           date(formatString: "MMMM DD, YYYY")
+          language
+          tags
         }
         fields {
           slug
         }
-        excerpt
+        plainText
       }
     }
   }
@@ -24,35 +30,47 @@ const queries = [
   {
     query,
     indexName: `challenges`,
-    transformer: ({ data }) => data.allMarkdownRemark.edges.map(({ node }) => node),
+    transformer: ({ data }) => data.allMarkdownRemark.edges.map(({ node }) => {
+      const { frontmatter, fields, plainText } = node
+      const { title, date, language, tags } = frontmatter
+      const { slug } = fields
+      return {
+        date,
+        language,
+        objectID: slug,
+        plainText,
+        slug,
+        tags,
+        title
+      }
+    }),
   }
 ];
 
 module.exports = {
   siteMetadata: {
-    title: 'Friday Challenges',
+    title: `Friday Challenges`,
     algolia: {
-      appId: process.env.ALGOLIA_APPID ? process.env.ALGOLIA_APPID : '',
-      searchOnlyApiKey: process.env.ALGOLIA_APIKEY
-        ? process.env.ALGOLIA_APIKEY
-        : '',
-      indexName: 'challenges'
+      appId: process.env.ALGOLIA_APPID,
+      searchOnlyApiKey: process.env.ALGOLIA_APIKEY,
+      indexName: `challenges`
     }
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
-    'gatsby-transformer-remark',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-remark-plaintext`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
     {
@@ -66,7 +84,7 @@ module.exports = {
       resolve: `gatsby-plugin-algolia`,
       options: {
         appId: process.env.ALGOLIA_APPID,
-        apiKey: process.env.ALGOLIA_APIKEY,
+        apiKey: process.env.ALGOLIA_ADMIN_APIKEY,
         queries
       }
     }
