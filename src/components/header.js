6985041844e57896ec56aaf6e7b6d52df7b420f7
Spencer Sanchez <spsanchez@wayfair.com>
2018-12-14 13:45:14 -0500

    Add basic auth to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'gatsby';
+const netlifyIdentity = require("netlify-identity-widget");
 
-const Header = ({ siteTitle }) => {
+class Header extends Component {
+  componentDidMount(){
+    netlifyIdentity.init({
+      APIUrl: "https://www.friday-challenge.com/.netlify/identity"
+    });
+  }
+
+  render() {
+    const { siteTitle } = this.props
     return (
-    <div
-      style={{
-        background: 'rebeccapurple',
-        marginBottom: '1.45rem'
-      }}
-    >
       <div
         style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '1.45rem 1.0875rem'
+          background: 'rebeccapurple',
+          marginBottom: '1.45rem'
         }}
       >
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: 'white',
-              textDecoration: 'none'
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
+
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem'
+          }}
+        >
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none'
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          <div data-netlify-identity-menu></div>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 };
 
 export default Header;
