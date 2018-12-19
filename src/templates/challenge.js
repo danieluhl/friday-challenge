de218402db9989f67ca50883b53b1770bc61d178
Spencer Sanchez <spsanchez@wayfair.com>
2018-12-18 22:08:08 -0500

    Revert "Remove `dompurify`"
    This reverts commit 1fe16f92739a3f501c687a0a295a4d6a6f3b7199.

--- src/templates/challenge.js
+++ src/templates/challenge.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import createDOMPurify from 'dompurify';
 
 import Layout from '../components/layout';
 import Tag from '../components/tag';
@@ -8,11 +9,15 @@ const ChallengePage = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark;
   const { title, tags } = frontmatter;
   const tagText = tags.split(/\s*,\s*/);
+  const purify = typeof window !== 'undefined' ? createDOMPurify(window) : null;
+  const sanitizedHtml = purify
+    ? purify.sanitize(html)
+    : 'could not purify html';
   return (
     <Layout>
       <h1>{title}</h1>
       {tagText.map((text) => <Tag key={text} text={text} />)}
-      <section dangerouslySetInnerHTML={{ __html: html }} />
+      <section dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
     </Layout>
   );
 };
