3b9b745f76bf83f4b2127cc94dee4fbbb1512b6c
Dan Uhl <duhl@wayfair.com>
2019-02-28 08:26:57 -0500

    shows language filters at all times

--- src/components/search.js
+++ src/components/search.js
@@ -1,18 +1,11 @@
-import React from 'react'
-import {
-  Highlight,
-  Hits,
-  InstantSearch,
-  SearchBox,
-  Stats,
-  RefinementList,
-} from 'react-instantsearch-dom'
+import React from 'react';
+import { Highlight, Hits, InstantSearch, SearchBox, Stats, RefinementList } from 'react-instantsearch-dom';
 
-// @TODO remove this - it's canned styling
-import './algolia.css'
+import './algolia.css';
+import './search.css';
 
 // Min number of characters needed to trigger search results
-const MIN_CHAR_COUNT = 0
+const MIN_CHAR_COUNT = 0;
 
 const Result = ({ hit }) => (
   <a style={{ marginTop: '10px' }} href={hit.slug}>
@@ -22,47 +15,45 @@ const Result = ({ hit }) => (
     <Highlight attribute="date" hit={hit} />
     <Highlight attribute="tags" hit={hit} />
     <small>{hit.date}</small>
-    <div>{hit.tags}</div>
   </a>
-)
+);
 
 class Search extends React.Component {
   state = {
-    showResults: false,
-  }
+    showResults: false
+  };
 
   handleChange = e => {
     this.setState({
-      showResults: e.target.value.length > MIN_CHAR_COUNT,
-    })
-  }
+      showResults: e.target.value.length > MIN_CHAR_COUNT
+    });
+  };
 
-  handleReset = () => this.setState({ showResults: false })
+  handleReset = () => this.setState({ showResults: false });
 
   render() {
-    const { appId, apiKey, indexName } = this.props
+    const { appId, apiKey, indexName } = this.props;
     return (
       <InstantSearch appId={appId} apiKey={apiKey} indexName={indexName}>
         <div
           style={{
             margin: '0 auto',
             maxWidth: 960,
-            padding: '1.45rem 1.0875rem',
+            padding: '1.45rem 1.0875rem'
           }}
         >
+          <RefinementList attribute="language" />
           <SearchBox onChange={this.handleChange} onReset={this.handleReset} />
-
           {this.state.showResults && (
             <div>
-              <RefinementList attribute="language" />
               <Hits hitComponent={Result} />
               <Stats />
             </div>
           )}
         </div>
       </InstantSearch>
-    )
+    );
   }
 }
 
-export default Search
+export default Search;
